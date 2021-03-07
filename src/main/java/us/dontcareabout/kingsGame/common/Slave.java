6ba16a74e49b2ccba2a210c26dc2e4f9f29a3773
MontyPan <monty.pan@dontcareabout.us>
2021-03-07 16:33:51 +0800

    增加：Slave

---
+++ src/main/java/us/dontcareabout/kingsGame/common/Slave.java
@@ -0,0 +1,49 @@
+package us.dontcareabout.kingsGame.common;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.InputEvent;
+import java.awt.image.BufferedImage;
+
+public class Slave {
+	private final Robot robot;
+
+	public Slave() throws Exception {
+		robot = new Robot();
+	}
+
+	public void click(XY xy) {
+		robot.mouseMove(xy.x, xy.y);
+		robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+		robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+	}
+
+	public Color getColor(XY xy) {
+		return robot.getPixelColor(xy.x, xy.y);
+	}
+
+	/**
+	 * @param ms 不能超過 60000ms
+	 */
+	public void wait(int ms) {
+		robot.delay(ms);
+	}
+
+	/**
+	 * @param second 不能超過 60s
+	 */
+	public void sleep(int second) {
+		wait(second * 1000);
+	}
+
+	public BufferedImage screenShot() {
+		return robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+	}
+
+	public BufferedImage screenShot(Rect area) {
+		Rectangle screenRect = new Rectangle(area.location.x, area.location.y, area.size.x, area.size.y);
+		return robot.createScreenCapture(screenRect);
+	}
+}
