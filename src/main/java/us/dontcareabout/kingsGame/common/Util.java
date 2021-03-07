9c02c7ea8f639cd598573eb04785b760f7d969ba
MontyPan <monty.pan@dontcareabout.us>
2021-03-07 16:34:16 +0800

    增加：Util

---
+++ src/main/java/us/dontcareabout/kingsGame/common/Util.java
@@ -0,0 +1,26 @@
+package us.dontcareabout.kingsGame.common;
+
+import java.awt.image.BufferedImage;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+	public static String nowText() { return dateFormat.format(new Date()); }
+
+	public static long now() { return new Date().getTime(); }
+
+	public static boolean compare(BufferedImage a, BufferedImage b) {
+		if (a.getWidth() != b.getWidth()) { return false; }
+		if (a.getHeight() != b.getHeight()) { return false; }
+
+		for (int x = 0; x < a.getWidth(); x++) {
+			for (int y = 0; y < a.getHeight(); y++) {
+				if (a.getRGB(x, y) != b.getRGB(x, y)) { return false; }
+			}
+		}
+
+		return true;
+	}
+}
