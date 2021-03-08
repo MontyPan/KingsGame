67333d506c63a40d5064ce0310c506fa06858e0d
MontyPan <monty.pan@dontcareabout.us>
2021-03-08 23:06:05 +0800

    增加：「加入聯盟式」的晉升處理、觀察模式與自動模式

--- src/main/java/us/dontcareabout/kingsGame/qtd/QTD.java
+++ src/main/java/us/dontcareabout/kingsGame/qtd/QTD.java
@@ -2,6 +2,8 @@ package us.dontcareabout.kingsGame.qtd;
 
 import java.awt.image.BufferedImage;
 
+import javax.swing.JOptionPane;
+
 import us.dontcareabout.kingsGame.common.Rect;
 import us.dontcareabout.kingsGame.common.Slave;
 import us.dontcareabout.kingsGame.common.Util;
@@ -14,13 +16,14 @@ public class QTD {
 
 	private static final int crewWidth = 140;
 	private static final int updateEnable = -4444444;
-	private static final int updateDisable = -8684165;
-
-	private static final int[] updateIndexOrder = {2, 0, 3, 4, 6, 1, 5};
+	private static final int rebirthBloodColor = -6547416;
+	private static final int[] updateIndexOrder = {2, 0, 3, 5, 6, 4, 1};
 
 	private static final XY rebirth = new XY(100, 340);
 	private static final XY rebirthConfirm = new XY(335, 495);
+	private static final XY rebirthJoinConfirm = new XY(70, 495);
 	private static final XY rebirthEnd = new XY(450, 400);
+	private static final XY rebirthBlood = new XY(80, 320);
 	private static final XY crew1 = new XY(25, 520);
 	private static final Rect levelArea = new Rect(440, 55, 35, 15);
 
@@ -31,16 +34,16 @@ public class QTD {
 
 	private BufferedImage preLvImg;
 
-	private QTD() throws Exception {
-		slave = new Slave();
+	private QTD(boolean observeMode) throws Exception {
+		System.out.println("觀察模式：" + observeMode);
 
+		slave = new Slave();
 		preLvImg = slave.screenShot(levelArea);
 
 		while(true) {
 			slave.sleep(1);
 
-			if (Util.now() - upgradeTime > upgradeSlice) {
-				log("升級時間！ ");	//Delete
+			if (!observeMode && Util.now() - upgradeTime > upgradeSlice) {
 				for (int i : updateIndexOrder) {
 					XY crewXY = getCrewXY(i);
 					while (isCanUpgrade(crewXY)) {
@@ -55,12 +58,17 @@ public class QTD {
 			if (Util.now() - levelCheckTime > lvCheckSlice) {
 				BufferedImage nowLvImg = slave.screenShot(levelArea);
 				boolean result = Util.compare(preLvImg, nowLvImg);
-				log("重生檢查！ " + result);	//Delete
 				if (!result) {
 					preLvImg = nowLvImg;
 					levelCheckTime = Util.now();
+					continue;
+				}
+
+				if (observeMode) {
+					levelCheckTime = Util.now();
+					JOptionPane.showMessageDialog(null, "卡關了，大佬？");
 				} else {
-					log("重生啦～～～～ ");	//Delete
+					Util.log("重生啦～～～");
 					doRebirth();
 				}
 			}
@@ -76,21 +84,21 @@ public class QTD {
 		return slave.getColor(xy).getRGB() > updateEnable;
 	}
 
+	private boolean isJoinRebirth() {
+		return slave.getColor(rebirthBlood).getRGB() == rebirthBloodColor;
+	}
+
 	private void doRebirth() {
 		slave.click(rebirth);
 		slave.sleep(5);
-		slave.click(rebirthConfirm);
+		slave.click(isJoinRebirth() ? rebirthJoinConfirm : rebirthConfirm);
 		slave.sleep(5);
 		slave.click(rebirthEnd);
 	}
 
-	private void log(String message) {
-		System.out.print(Util.nowText() + " : ");
-		System.out.println(message);
-	}
-
 	@SuppressWarnings("unused")
 	public static void main(String[] args) throws Exception {
-		new QTD();
+		int type = JOptionPane.showConfirmDialog(null, "觀察模式？", "", JOptionPane.YES_NO_OPTION);
+		new QTD(type == 0);
 	}
 }
