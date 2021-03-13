3c907b6e32a02f0c8d0a1e3fb3593711e5441fc9
MontyPan <monty.pan@dontcareabout.us>
2021-03-13 19:56:14 +0800

    增加：colorDiff()

--- src/main/java/us/dontcareabout/kingsGame/common/Util.java
+++ src/main/java/us/dontcareabout/kingsGame/common/Util.java
@@ -1,5 +1,6 @@
 package us.dontcareabout.kingsGame.common;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,6 +16,14 @@ public class Util {
 	public static String nowText() { return dateFormat.format(new Date()); }
 
 	public static long now() { return new Date().getTime(); }
+
+	public static double colorDiff(Color a, Color b) {
+		return Math.sqrt(
+			2 * Math.pow(a.getRed() - b.getRed(), 2) +
+			4 * Math.pow(a.getGreen() - b.getGreen(), 2) +
+			3 * Math.pow(a.getBlue() - b.getBlue(), 2)
+		);
+	}
 
 	public static boolean compare(BufferedImage a, BufferedImage b) {
 		if (a.getWidth() != b.getWidth()) { return false; }
