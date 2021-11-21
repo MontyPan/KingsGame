86c07ae35161b3db98740951baa528a6370fae24
MontyPan <monty.pan@dontcareabout.us>
2021-11-21 11:39:41 +0800

    修正：Slave.sleep() 導致其他 thread 無法快速運作的問題

--- src/main/java/us/dontcareabout/kingsGame/common/TaskManager.java
+++ src/main/java/us/dontcareabout/kingsGame/common/TaskManager.java
@@ -2,22 +2,25 @@ package us.dontcareabout.kingsGame.common;
 
 import java.util.ArrayList;
 import java.util.PriorityQueue;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public 	class TaskManager {
-	private ExecutorService service = Executors.newSingleThreadExecutor();
+	private ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
 	private ArrayList<Task> list = new ArrayList<>();
 	private PriorityQueue<Task> queue = new PriorityQueue<>((t1, t2) -> t1.priority.compareTo(t2.priority));
 
 	public void start() {
-		service.execute(() -> {
-			while(true) {
-				Slave.call().sleep(2);
+		//Slave.sleep() 會繼續霸佔 thread 運行，其他 thread 就沒得跑
+		//所以改成 ScheduledExecutorService 才是正道
+		service.scheduleAtFixedRate(
+			() -> {
 				checkList();
 				resolveQueue();
 			}
-		});
+			, 0, 2, TimeUnit.SECONDS
+		);
 	}
 
 	public void add(Task t) {
