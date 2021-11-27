8e02ae7c4e340043c9ff7ea17a0daa350ab31d96
MontyPan <monty.pan@dontcareabout.us>
2021-11-27 12:01:09 +0800

    增加：ImageSet

---
+++ src/main/java/us/dontcareabout/kingsGame/shared/qtd/ImageSet.java
@@ -0,0 +1,19 @@
+package us.dontcareabout.kingsGame.shared.qtd;
+
+public class ImageSet {
+	public String stage;
+	public String screen;
+
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	public String getScreen() {
+		return screen;
+	}
+	public void setScreen(String screen) {
+		this.screen = screen;
+	}
+}
