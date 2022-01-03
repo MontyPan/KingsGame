d1b0e9e526a3bdc5c6793db32cb2c9da49d57284
MontyPan <monty.pan@dontcareabout.us>
2022-01-03 22:13:24 +0800

    增加：pause() / resume()

--- src/main/java/us/dontcareabout/kingsGame/common/TaskManager.java
+++ src/main/java/us/dontcareabout/kingsGame/common/TaskManager.java
@@ -6,10 +6,15 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import us.dontcareabout.kingsGame.server.Logger;
+
 public 	class TaskManager {
 	private ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
 	private ArrayList<Task> list = new ArrayList<>();
 	private PriorityQueue<Task> queue = new PriorityQueue<>((t1, t2) -> t1.priority.compareTo(t2.priority));
+
+	private boolean pauseFlag;
+	private long pauseTime;
 
 	public void start() {
 		//Slave.sleep() 會繼續霸佔 thread 運行，其他 thread 就沒得跑
@@ -27,11 +32,26 @@ public 	class TaskManager {
 		list.add(t);
 	}
 
+	public void pause() {
+		Logger.log("暫停中");
+		pauseFlag = true;
+		pauseTime = Util.now();
+	}
+
+	public void resume() {
+		int pausePeriod = (int)((Util.now() - pauseTime) / 1000);
+		Logger.log("恢復（delay : " + pausePeriod + "）" );
+		list.forEach(task -> task.delay(pausePeriod));
+		pauseFlag = false;
+	}
+
 	public void shutdown() {
 		service.shutdown();
 	}
 
 	private void checkList() {
+		if (pauseFlag) { return; }
+
 		long now = Util.now();
 
 		for (Task t : list) {
